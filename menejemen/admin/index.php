c5b7a8a620a81cec8611bac6050ac8a70d70124d
SuperBase <tutorialsuper.cs@gmail.com>
2017-07-04 11:36:26 +0700

    update font awesome fixed

--- menejemen/admin/index.php
+++ menejemen/admin/index.php
@@ -29,11 +29,10 @@
   <title>SIANTRO UGM | Dashboard</title>
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="../assets/font/css/font-awesome.min.css">
+  <link rel="stylesheet" href="../assets/font/ionicons.min.css">
   <link rel="stylesheet" href="../assets/dist/css/AdminLTE.min.css">
   <link rel="stylesheet" href="../assets/dist/css/skins/_all-skins.min.css">
-  <!-- <link rel="stylesheet" href="../assets/plugins/iCheck/flat/blue.css"> -->
   <link rel="stylesheet" href="../assets/plugins/morris/morris.css">
   <link rel="stylesheet" href="../assets/plugins/jvectormap/jquery-jvectormap-1.2.2.css">
   <link rel="stylesheet" href="../assets/plugins/datepicker/datepicker3.css">
