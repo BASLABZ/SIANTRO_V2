95a6c0965692a268ff8efc3783eedfee80f5d4dd
SuperBase <tutorialsuper.cs@gmail.com>
2017-07-04 22:01:54 +0700

    update add pengguna

--- menejemen/admin/master/pengguna/add.php
+++ menejemen/admin/master/pengguna/add.php
@@ -43,92 +43,98 @@
   <section class="content">
     <div class="">
           <div class="box box-default">
-        <div class="box-header with-border">
-          <h3 class="box-title">Tambah Data Pengguna</h3>
-          <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
-            <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-remove"></i></button>
+          <div class="box-header with-border">
+            <h3 class="box-title">Tambah Data Pengguna</h3>
+            <div class="box-tools pull-right">
+              <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+              <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-remove"></i></button>
+            </div>
           </div>
-        </div>
         <div class="box-body">
           <form id="defaultForm" class="role" method="POST" enctype="multipart/form-data">
-            <div class="form-group row">
-              <div class="col-md-12">
-                <label class="col-md-2">Nama Operator</label>
-              <div class="col-md-4">
-                <input type="text" required class="form-control" name="operator_name" placeholder="Nama Operator">
-              </div>
+            <div class="row">
+              <div class="col-md-6">
+                <label class="col-md-3">Nama Lengkap</label>
+                <div class="col-md-8">
+                  <div class="form-group">
+                   <input type="text" required class="form-control" name="operator_name" placeholder="Nama Operator">
+                </div>
+                </div>
               </div>
             </div>
-            <div class="form-group row">
+            <div class="row">
               <div class="col-md-6">
-              <label class="col-md-4">Username</label>
-              <div class="col-md-8">
-                <input type="text" required class="form-control" name="operator_username" placeholder="Username Operator">
-              </div>
+                <div class="form-group">
+                  <label class="col-md-3">Username</label>
+                  <div class="col-md-8">
+                    <input type="text" required class="form-control" name="operator_username" placeholder="Username Operator">
+                  </div>
+                </div>
               </div>
               <div class="col-md-6">
-              <label class="col-md-4">Password</label>
-              <div class="col-md-8">
-                <input type="password" required class="form-control" name="operator_password" placeholder="Password Operator">
-              </div>
+                <div class="form-group">
+                  <label class="col-md-3">Password</label>
+                  <div class="col-md-8">
+                     <input type="password" required class="form-control" name="operator_password" placeholder="Password Operator">
+                  </div> 
+                </div>
               </div>
             </div>
-            <div class="form-group row">
+            <hr>
+            <div class="row">
               <div class="col-md-6">
-              <label class="col-md-4">Tempat Lahir</label>
-              <div class="col-md-8">
-               <textarea class="form-control" name="operator_placeofbirth" required="" placeholder="Tempat Lahir">
-                 
-               </textarea>
-              </div>
+                <label class="col-md-3">Tempat Lahir</label>
+                 <div class="col-md-8">
+                 <textarea class="form-control" name="operator_placeofbirth" required="" placeholder="Tempat Lahir"></textarea>
+                </div>
               </div>
               <div class="col-md-6">
-              <label class="col-md-4">Tanggal Lahir</label>
-              <div class="col-md-8">
-                <input type="date" required class="form-control" name="operator_dateofbirth" placeholder=" Tanggal Lahir">
-              </div>
+                 <label class="col-md-3">Tanggal Lahir</label>
+                 <div class="col-md-8">
+                   <input type="date" required class="form-control" name="operator_dateofbirth" placeholder=" Tanggal Lahir">
+                 </div>
               </div>
             </div>
-            <div class="col-md-6">
-              <label class="col-md-4">Jenis Kelamin</label>
-
-              <div class="col-md-8">
-              <select class="form-control" name="operator_gender">
+            <hr>
+            <div class="row">
+              <div class="col-md-6">
+                <label class="col-md-3">Alamat</label>
+                <div class="col-md-8">
+                   <textarea class="form-control" name="operator_address" required=""></textarea>
+                </div>
+              </div> 
+              <div class="col-md-6">
+                <label class="col-md-3">No Telp</label>
+                <div class="col-md-8">
+                  <input type="number" required class="form-control" name="operator_phonenumber" placeholder="No Telp Trainer">
+                </div>
+              </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-md-6">
+                <label class="col-md-3">Jenis Kelamin</label>
+                <div class="col-md-8">
+                  <select class="form-control" name="operator_gender">
                 <option value="">Pilih Jenis Kelamin</option>
                 <option value="Pria">Pria</option>
                 <option value="Wanita">Wanita</option>
               </select>
+                </div>
               </div>
+              <div class="col-md-6">
+                <label class="col-md-3">Email</label>
+                <div class="col-md-8">
+                  <input type="email" class="form-control" name="operator_email" required="" placeholder="email">
+                </div>
               </div>
             </div>
-             <div class="form-group row">
+            <hr>
+            <div class="row">
               <div class="col-md-6">
-              <label class="col-md-4">Alamat</label>
-              <div class="col-md-8">
-                <textarea class="form-control" name="operator_address" required=""></textarea>
-              </div>
-              </div>
-              <div class="col-md-6">
-              <label class="col-md-4">No.Telp</label>
-              <div class="col-md-8">
-                <input type="number" required class="form-control" name="operator_phonenumber" placeholder="No Telp Trainer">
-              </div>
-              </div>
-            </div>
-            <div class="form-group row">
-              <div class="col-md-6">
-              <label class="col-md-4">Email</label>
-              <div class="col-md-8">
-                <input type="email" class="form-control" name="operator_email" required="" placeholder="email">
-              </div>
-              </div>
-
-            <div class="form-group row">
-              <div class="col-md-6">
-              <label class="col-md-4">Agama</label>
-              <div class="col-md-8">
-               <select class="form-control" name="operator_religion" required="required">
+                <label class="col-md-3">Agama</label>
+                <div class="col-md-8">
+                  <select class="form-control" name="operator_religion" required="required">
                  <option value="">Pilih Agama</option>
                  <option value="Islam">Islam</option>
                  <option value="Kristen">Kristen</option>
@@ -137,50 +143,45 @@
                  <option value="Protestan">Protestan</option>
                  <option value="Lainnya">Lainnya</option>
                </select>
-              </div>
-              </div>
-              
-           
+                </div>
+                </div>
               <div class="col-md-6">
-              <label class="col-md-4">Website</label>
-              <div class="col-md-8">
-                <input type="text" required class="form-control" name="operator_website" placeholder="http:">
+                <label class="col-md-3">Website</label>
+                <div class="col-md-8">
+                  <input type="text" required class="form-control" name="operator_website" placeholder="http:">
+                </div>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-md-6">
+                <label class="col-md-3">Jabatan</label>
+                <div class="col-md-8">
+                  <input type="text" class="form-control" name="operator_position" required="" placeholder="Jabatan">
+                </div>
               </div>
+              <div class="col-md-6">
+                <label class="col-md-3">Upload</label>
+                <div class="col-md-8">
+                  <input type="file" required name="operator_image">
+                </div> 
               </div>
             </div>
-             <div class="form-group row">
-            <div class="col-md-6">
-              <label class="col-md-4">Jabatan</label>
-              <div class="col-md-8">
-                <input type="text" class="form-control" name="operator_position" required="" placeholder="Jabatan">
-              </div>
-            </div>
-            <!-- <div class="col-md-6">
-              <label class="col-md-4">Pertanyaan keamanan/label>
-              <div class="col-md-8">
-                <input type="text" class="form-control" name="operator_hint" required="" placeholder="Jabatan">
-              </div>
-              </div> -->
-            <div class="col-md-6">
-              <label class="col-md-4">Upload</label>
-              <div class="col-md-8">
-                <input type="file" required name="operator_image">
-              </div>
-              </div>
-            </div>
-            <div class="col-md-6">
-              <label class="col-md-4">Status Operator </label>
-              <div class="col-md-8">
-                <select class="form-control" name="operator_status" required="">
+            <hr>
+            <div class="row">
+              <div class="col-md-6">
+                <label class="col-md-3">Status</label>
+                <div class="col-md-8">
+                  <select class="form-control" name="operator_status" required="">
                   <option>Active</option>
                   <option>Inactive</option>
                 </select>
-              </div>
+                </div>
               </div>
               <div class="col-md-6">
-              <label class="col-md-4">Level Operator</label>
+                <label class="col-md-3">Level Operator</label>
                 <div class="col-md-8">
-                <select class="form-control" name="operator_level" required="">
+                  <select class="form-control" name="operator_level" required="">
                   <?php 
                       $sqlevel="SELECT level_id, level_name from ref_level";
                       $runsqlevel=mysql_query($sqlevel);
@@ -188,16 +189,20 @@
                               $var_idLev=$datalevel['level_id'];
                               $var_namaLev=$datalevel['level_name'];
                    ?>
-                 <option value="<?php echo $var_idLev?>"><?php echo $var_namaLev ?></option>
-                  <?php 
-                                    }//tutup dari while
-
-                                  ?>
+                    <option value="<?php echo $var_idLev?>"><?php echo $var_namaLev ?></option>
+                  <?php }  ?>
                 </select>
                 </div>
               </div>
-            <div class="form-group">
-              <button class="btn btn-primary btn-sm pull-right" type="submit" name="simpan"><span class="fa fa-save"></span> Simpan</button>
+            </div>
+            <hr>
+            <div class="row">
+              <div class="col-md-3"></div>
+              <div class="col-md-8">
+                    <div class="form-group pull-right">
+                      <button class="btn btn-primary btn-sm" type="submit" name="simpan"><span class="fa fa-save"></span> Simpan</button>
+                    </div>
+              </div>
             </div>
           </form>
        
